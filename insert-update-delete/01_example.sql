60b7ab3b01854750854f69dc14816fa205f1a426
Bolutife Alli <tifealli28@gmail.com>
2025-03-08 11:12:06 -0600

    Update 01_example.sql

---
+++ insert-update-delete/01_example.sql
@@ -0,0 +1,3 @@
+Use Northwind;
+Insert into suppliers (CompanyName, ContactName, ContactTitle, Address, City, Country)
+Values ('Zumies', 'John Otto', 'Team Lead', '2526 Burbon Street', 'Brooklyn', 'United States');
